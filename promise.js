919670a8034a114d9a09ec16990fc7e4efb8165d
DAS <david@indyblue.com>
2016-10-22 07:02:55 -0400

    make promise more of a swiss army tool - move round into promise - create extend module in promise - create getValue in promise create pdf-style to house all levels of styling - page, section, block, font
    comment out logger in ttf stuff
    add test code in t.js to get extend working right

--- promise.js
+++ promise.js
@@ -38,6 +38,51 @@ var fno = function() {
 	};
 	return t;
 };
+
 module.exports = {
-	new: () => new fno()
+	new: () => new fno(),
+	extend: fnExtend,
+	round: fnRound,
+	getValue: fnGetValue
 };
+
+function fnExtend(){
+	var retval = {};
+	if(arguments.length>0) retval = arguments[0];
+	for(var i=1;i<arguments.length;i++){
+		var arg = arguments[i];
+		if(typeof arg!='object') continue;
+		var argKeys = Object.keys(arg);
+		for(var j=0;j<argKeys.length;j++){
+			var key = argKeys[j];
+			var val = arg[key];
+			var descr = Object.getOwnPropertyDescriptor(arg,key);
+			if(typeof descr.get!='undefined' || typeof descr.set!='undefined'){
+				var val = {enumerable:true,configurable:true};
+				if(typeof descr.get!='undefined') val.get = descr.get;
+				if(typeof descr.set!='undefined') val.set = descr.set;
+				Object.defineProperty(retval, key, val);
+			}
+			else retval[key] = val;
+		}
+	}
+	return retval;
+}
+
+
+function fnRound(x, n) {
+	var f = Math.pow(10,n)
+	var r = x*f;
+	var r2 = Math.round(r);
+	var r3 = r2/f;
+	return r3;
+}
+
+function fnGetValue(obj, key, def) {
+	if(typeof obj!=='object') return def;
+	if(typeof obj[key]!== 'undefined')
+		return obj[key];
+	else return def;
+
+}
+
